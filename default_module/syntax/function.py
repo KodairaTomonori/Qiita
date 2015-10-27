55dc710dd41b6f70e03a30f9170dee517d84145e
KODAIRATOMONORI <kodairatomonori@GN.local>
2015-10-28 02:48:24 +0900

    http://qiita.com/Kodaira_/items/c42c8d7ab27865e657d3

---
+++ default_module/syntax/function.py
@@ -0,0 +1,15 @@
+
+def addition(num_a: 'int', num_b: 'int', flag:'bool'=True):
+    '''
+    この関数は、num_a + num_bを計算して返すプログラムです。
+    flagをTrueにすると結果を見やすくprintしてくれます。
+    '''
+    if flag: print('{} + {} = {}'.format(num_a, num_b, num_a + num_b) )
+    return num_a + num_b
+
+
+if __name__ == '__main__':
+    result = addition(1234, 4321)
+    print('result:', result) 
+    print('引数の情報: ', addition.__annotations__)
+    print('関数の説明: ', addition.__doc__)
